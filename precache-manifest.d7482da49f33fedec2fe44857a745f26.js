c1dc6acf4367541fad02d29b4bbb35384aba7b48
Mateusz Blajda <mateuszblajda99@gmail.com>
2020-10-25 13:45:31 +0100

    Updates

---
+++ precache-manifest.d7482da49f33fedec2fe44857a745f26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f3b01306a9231edf073a8f6df0d4dc9",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "91ca25ef619da13b86b6",
+    "url": "/weather/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "91ca25ef619da13b86b6",
+    "url": "/weather/static/js/2.3a046a73.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/weather/static/js/2.3a046a73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5dac25cefaa2b4e24e2",
+    "url": "/weather/static/js/main.52786f71.chunk.js"
+  },
+  {
+    "revision": "b83e05f3b1e38480a3ef",
+    "url": "/weather/static/js/runtime-main.d16f45af.js"
+  }
+]);
